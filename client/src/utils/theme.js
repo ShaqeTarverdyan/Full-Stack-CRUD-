e89afe035fd32391c32e80217fdd96f80d0f8635
ShaqeTarverdyan <tshaqe@gmail.com>
2020-10-07 17:05:32 +0400

    Create global styles

---
+++ client/src/utils/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+    colors: {
+      main: '#3b7ba8',
+      mainDark: '#141f40',
+      mainLight: '#3888a6',
+      mainLighter: '#88d5f2',
+      textColor: '#333',
+      whiteColor: '#fff',
+      shadow: 'rgba(0,0,0,.2)',
+      errorRed: '#de2c2c',
+    }
+  };
+  
+  export default theme;
