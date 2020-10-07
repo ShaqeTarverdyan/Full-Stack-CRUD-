7af250cb99b4bff6d0f50f6f24af98a38a5acc23
ShaqeTarverdyan <tshaqe@gmail.com>
2020-10-07 18:00:24 +0400

    create route simple carcas

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,22 @@
-import React, {useState} from 'react';
-import Axios from 'axios';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+
+//components
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard'
 
 const App = () => {
-  const [usernameReg, setUserNameReg] = useState('');
-  const [passwordReg, setPasswordReg] = useState('');
-
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const [loginStatus, setloginStatus] = useState('');
-
-
-  const register = () => {
-    Axios.post("http://localhost:3001/register", {
-      username: usernameReg,
-      password: passwordReg,
-    }).then((response) => {
-      console.log(response);
-    })
-  };
-
-  const login = () => {
-    Axios.post("http://localhost:3001/login", {
-      username: username,
-      password: password,
-    }).then((response) => {
-      if(response.data.message) {
-        setloginStatus(response.data.message)
-      }  else {
-        setloginStatus(response.data[0].username)
-      }
-    })
-  }
 
   return (
-    <>
-    <div>
-      <h1>Register</h1>
-      <label>username</label>
-      <input 
-        type="text"
-        onChange={(e) => {
-          setUserNameReg(e.target.value)
-        }}
-      />
-      <label>[password]</label>
-      <input 
-        type="text"
-        onChange={(e) => {
-          setPasswordReg(e.target.value)
-        }}
-      />
-      <button onClick={register}>Register</button>
-    </div>
-    <div>
-      <h1>Login</h1>
-      <label>username</label>
-      <input 
-        type="text"
-        onChange={(e) => {
-          setUsername(e.target.value)
-        }}
-      />
-      <label>password</label>
-      <input 
-        type="text"
-        onChange={(e) => {
-          setPassword(e.target.value)
-        }}
-      />
-      <button onClick={login}>Login</button>
-    </div>
-    <h3>{loginStatus}</h3>
-  </>
+    <BrowserRouter>
+      <div>
+        <Navbar/>
+        <Switch>
+            <Route exact path="/" component={Dashboard}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
