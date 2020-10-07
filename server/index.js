37f56fe174cc2809a255f466e005b289390ae903
ShaqeTarverdyan <tshaqe@gmail.com>
2020-10-07 16:59:49 +0400

    init register and login functionalities in basic form on server side

--- server/index.js
+++ server/index.js
@@ -1,17 +1,50 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
     user: 'root',
     host: '127.0.0.1',
     password: 'admin',
-    database: 'login_test'
+    database: 'hackTech_intern'
 });
+
+app.post("/register", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    db.query(
+        "INSERT INTO admins (username, password) VALUES (?,?)",
+        [username, password],
+        (err, result) => {
+            console.log(result);
+        }
+    )
+});
+
+app.post("/login", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    db.query(
+        "SELECT * FROM  admins WHERE username = ? AND password = ?",
+        [username, password],
+        (err, result) => {
+            if(err) res.send({error: err});
+            if(result.length > 0) {
+                res.send(result);
+            } else {
+                res.send({message: 'Wrong combination'})
+            }
+        }
+    )
+})
+
 
 app.listen(3001, () => {
     console.log('runnung system...');
+    
 });
