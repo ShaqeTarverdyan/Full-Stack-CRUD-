8b0514b58e2ec760294ffc1035c54fb045319157
ShaqeTarverdyan <tshaqe@gmail.com>
2020-10-08 17:10:07 +0400

    add new routes on client

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,10 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 //components
 import Navbar from './components/layout/Navbar';
-import Dashboard from './components/dashboard'
+import Dashboard from './components/dashboard';
+import News from './components/newsPage';
+import LogIn from './components/authentication/LogIn';
+import SignUp from './components/authentication/SignUp'
 
 const App = () => {
 
@@ -14,6 +17,9 @@ const App = () => {
         <Navbar/>
         <Switch>
             <Route exact path="/" component={Dashboard}/>
+            <Route path="/news" component={News}/>
+            <Route path="/logIn" component={LogIn}/>
+            <Route path="/signUp" component={SignUp}/>
         </Switch>
       </div>
     </BrowserRouter>
