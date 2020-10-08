bd5d10b9d86ea49d91a68fd56fa6b3f0ef975386
ShaqeTarverdyan <tshaqe@gmail.com>
2020-10-08 17:10:53 +0400

    some changes  on signup component

--- client/src/components/authentication/SignUp.js
+++ client/src/components/authentication/SignUp.js
@@ -1,38 +1,60 @@
 import React, { useState } from 'react';
 import Axios from 'axios';
 
+import { Container, FormWrapper } from '../../generalStyles';
+
 const SignUp = () => {
     const [usernameReg, setUserNameReg] = useState('');
     const [passwordReg, setPasswordReg] = useState('');
+    const [confirmPassword, setConfirmpassword] = useState('')
     
     const register = () => {
         Axios.post("http://localhost:3001/register", {
           username: usernameReg,
           password: passwordReg,
+          confirmPassword: confirmPassword,
         }).then((response) => {
-          console.log(response);
+          if(response.data.error) {
+              console.log('error from db', response.data.error.message)
+          }else {
+            console.log('yes')
+          }
+        }).catch(error => {
+            console.log(error.message)
         })
     };
     
     return (
-        <div>
-            <h1>Register</h1>
-            <label>username</label>
-            <input 
-                type="text"
-                onChange={(e) => {
-                setUserNameReg(e.target.value)
-                }}
-            />
-            <label>[password]</label>
-            <input 
-                type="text"
-                onChange={(e) => {
-                setPasswordReg(e.target.value)
-                }}
-            />
-            <button onClick={register}>Register</button>
-        </div>
+        <Container>
+            <FormWrapper>
+                <h1>Register</h1>
+                <label>username</label>
+                <input 
+                    name="username"
+                    type="text"
+                    onChange={(e) => {
+                        setUserNameReg(e.target.value)
+                    }}
+                />
+                <label>password</label>
+                <input 
+                    name="password"
+                    type="text"
+                    onChange={(e) => {
+                        setPasswordReg(e.target.value)
+                    }}
+                />
+                 <label>Confirm Password</label>
+                <input 
+                    name="confirmPassword"
+                    type="text"
+                    onChange={(e) => {
+                        setConfirmpassword(e.target.value)
+                    }}
+                />
+                <button onClick={register}>Register</button>
+            </FormWrapper>
+        </Container>
     )
 }
 
