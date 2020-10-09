58d2f12b0fff335af632354354ef2d23d803e6d9
ShaqeTarverdyan <tshaqe@gmail.com>
2020-10-09 18:02:14 +0400

    install libs

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,10 +4,7 @@ const router = express.Router();
 
 const adminController = require('../controllers/admin');
 
-router.post('/register', (req, res) => {
-    console.log('routes ', res)
-    
-});
-router.get('/register', adminController.registerNewAdmin);
+router.post('/register', adminController.registerNewAdmin);
+router.get('/register', adminController.getAdmins);
 
 module.exports = router;
