1d4257912c8fe60b671db31a75e0019aa7a0a4e6
ShaqeTarverdyan <tshaqe@gmail.com>
2020-10-09 18:55:40 +0400

    initiate redux adctions and reducer for admin

--- client/src/store/actions/authActions.js
+++ client/src/store/actions/authActions.js
@@ -1,30 +1,38 @@
 import { CONSTANTS } from './Constants';
 import Axios from 'axios';
 
-export const signUp = (newAdmin) => {
+export const signUp = (newAdmin, hisory) => {
     console.log('newAdmin', newAdmin)
-    const { username, email, password, role} = newAdmin;
+    const { firstname, lastname, email, password, role} = newAdmin;
     return (dispatch, getState) => {
-        console.log('signUped', newAdmin)
+        dispatch({type: CONSTANTS.SIGNUP_START})
         Axios.post("http://localhost:3001/register", {
-            username: username,
+            firstname: firstname,
+            lastname: lastname,
             email: email,
             password: password,
             role: role,
         }).then((response) => {
-          if(response.data.error) {
-              console.log('error from db', response.data.error.message)
-          }else {
-            console.log('yes')
-          }
+            console.log('result',response)
+            if(response.status === 200) {
+                dispatch({type: CONSTANTS.SIGNUP_SUCCESS});
+                hisory.push('/login')
+            }
         }).catch(error => {
-            console.log(error.message)
+            dispatch({type: CONSTANTS.SIGNUP_ERROR, payload: error.message})
         })
     }
 }
 
 export const logIn = (admin) => {
+    const { email, password } = admin;
     return (dispatch, getState) => {
         console.log('signedin', admin)
+        Axios.post("http://localhost:3001/login", {
+            email: email,
+            password: password,
+      }).then((response) => {
+        console.log(response)
+      })
     }
 }
--- client/src/store/reducers/authReducer.js
+++ client/src/store/reducers/authReducer.js
@@ -1,9 +1,26 @@
 import { CONSTANTS } from '../actions/Constants';
 
-const initialState = {}
+const initialState = {
+    loading: false,
+    eror: null,
+    user: {}
+}
 
-export default (state = initialState, actions) => {
+export default (state = initialState, {type, payload}) => {
     const newState = {...state};
-
-    return newState;
+    switch(type) {
+        case CONSTANTS.SIGNUP_START: {
+            return { ...newState, loading: true}
+        }
+        case CONSTANTS.SIGNUP_SUCCESS: {
+            return { ...newState, loading: false, error: null}
+        }
+        case CONSTANTS.SIGNUP_ERROR: {
+            console.log('error', payload)
+            return {...newState, loading: false, error: payload}
+        }
+        default: {
+			return newState
+		}
+    }
 }
