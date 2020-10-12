c45a385570abc1ca8e86b74cff0324846214ac57
ShaqeTarverdyan <tshaqe@gmail.com>
2020-10-12 12:54:23 +0400

    create errorPage and Loading component carcases on client side

---
+++ client/src/components/loader/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LoaderWrapper= styled.div``;
+
+
+const Loader = ({children}) => {
+    return (
+    <LoaderWrapper>Loading...</LoaderWrapper>
+    )
+}
+
+export default Loader;
