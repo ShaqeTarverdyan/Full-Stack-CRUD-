ace9f33cea6914f37acc12ddb5bc1214161ec678
ShaqeTarverdyan <tshaqe@gmail.com>
2020-10-13 19:47:43 +0400

    init isAuth middleware on news/server/routes

--- server/routes/news.js
+++ server/routes/news.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { body } = require('express-validator/check');
+const { body } = require('express-validator');
 const router = express.Router();
 const newsController = require('../controllers/news');
+const isAuth = require('../middleware/is-auth');
 
-router.get('/news-list', newsController.getallNews);
+router.get('/news-list', isAuth, newsController.getallNews);
 router.post(
         '/news',
+        isAuth,
         [
             body('title')
                 .trim()
@@ -15,9 +17,9 @@ router.post(
                 .notEmpty()
         ],
         newsController.addNews);
-router.put('/news/:newsId', newsController.updateNews);
-router.delete('/news/:newsId', newsController.deleteNews);
-router.get('/news-/:newsId', newsController.getCurrentNews);
+router.put('/news/:newsId', isAuth, newsController.updateNews);
+router.delete('/news/:newsId',isAuth, newsController.deleteNews);
+router.get('/news-/:newsId', isAuth, newsController.getCurrentNews);
 
 
 module.exports = router;
