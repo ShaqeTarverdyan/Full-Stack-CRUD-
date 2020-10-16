53ce97704b6fd927322b907018c6e67d94a6d622
ShaqeTarverdyan <tshaqe@gmail.com>
2020-10-16 19:02:35 +0400

    if admin is not loged history push login form in dashboard component

--- client/src/components/dashboard/index.js
+++ client/src/components/dashboard/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { useHistory }  from 'react-router-dom';
 
 
-const Dashboard = () => {
+const Dashboard = ({admin_id}) => {
+    let history = useHistory();
+
+
+    if(!admin_id) {
+        history.push("/login")
+    }
     return(
         <div>
             <h1>Welcome Dashboard:)</h1>
         </div>
     )
 }
-
-export default Dashboard;
+const mapStateToProps = state => {
+    return {
+        admin_id: state.auth.admin_id
+    }
+}
+export default connect(mapStateToProps)(Dashboard);
