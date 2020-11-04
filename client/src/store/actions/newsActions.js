dcc6d5b4a8c62cc99868c5d1b8648b76b1a4019e
ShaqeTarverdyan <tshaqe@gmail.com>
2020-11-04 16:26:56 +0400

    resolve some bugs

--- client/src/store/actions/newsActions.js
+++ client/src/store/actions/newsActions.js
@@ -233,7 +233,7 @@ export const getTypes = () => {
 }
 
 export const deleteImageFromBackend = (path, newsId) => {
-    alert("Do You Want really Delete File ???")
+    alert("This Image will deleted from all places")
     return dispatch => {
         dispatch({type: CONSTANTS.DELETE_IMAGE_START})
         Axios
@@ -257,7 +257,7 @@ export const deleteImageFromBackend = (path, newsId) => {
 }
 
 export const deleteFileFromBackend = (path, newsId) => {
-    alert("Do You Want really Delete File ???")
+    alert("This file will deleted from all places")
     return dispatch => {
         dispatch({type: CONSTANTS.DELETE_IMAGE_START})
         Axios
